7e5d0075cbc4bbe243fa9e722bf991030ba46109
Faciqui Doumbia <faciquidoumbia@gmail.com>
2023-02-18 00:08:35 +0100

    Adding POJOs. Closes #2

---
+++ src/main/java/com/example/sfcpetclinic/model/PetType.java
@@ -0,0 +1,14 @@
+package com.example.sfcpetclinic.model;
+
+public class PetType {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
