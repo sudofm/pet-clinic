59094b149552dd2cad78ddb984815a8c8fadedd2
Faciqui Doumbia <faciquidoumbia@gmail.com>
2023-02-17 20:56:26 +0100

    Initial project add

---
+++ src/main/java/com/example/sfcpetclinic/SfcPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.example.sfcpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfcPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfcPetClinicApplication.class, args);
+    }
+
+}
