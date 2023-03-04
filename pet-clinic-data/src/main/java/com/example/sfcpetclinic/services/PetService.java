08cfbcbe018cf23a424cac316eae20a400dce74c
Faciqui Doumbia <faciquidoumbia@gmail.com>
2023-03-04 13:40:49 +0100

    Refactor Interface services to common base interface. Closes #11

--- pet-clinic-data/src/main/java/com/example/sfcpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/com/example/sfcpetclinic/services/PetService.java
@@ -2,10 +2,5 @@ package com.example.sfcpetclinic.services;
 
 import com.example.sfcpetclinic.model.Pet;
 
-import java.util.Set;
-
-interface PetService {
-    Pet findById(Long id);
-    Pet save(Pet pet);
-    Set<Pet> findAll();
+interface PetService extends CrudService<Pet, Long> {
 }
--- pet-clinic-data/src/main/java/com/example/sfcpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/example/sfcpetclinic/services/VetService.java
@@ -2,10 +2,5 @@ package com.example.sfcpetclinic.services;
 
 import com.example.sfcpetclinic.model.Vet;
 
-import java.util.Set;
-
-public interface VetService {
-    Vet findById(Long id);
-    Vet save(Vet vet);
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long> {
 }
