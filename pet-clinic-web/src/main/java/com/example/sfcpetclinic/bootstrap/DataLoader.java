66136cc923b0803e2142d03d24341ba86a180e41
Faciqui Doumbia <faciquidoumbia@gmail.com>
2023-05-16 12:21:55 +0200

    Copy all static Resources from Spring pet clinic. Closes #17

--- pet-clinic-web/src/main/java/com/example/sfcpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/example/sfcpetclinic/bootstrap/DataLoader.java
@@ -20,14 +20,12 @@ public class DataLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Jean ");
         owner1.setLastName("Dupont");
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Omar ");
         owner2.setLastName("Sy");
 
@@ -36,14 +34,12 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loaded Owners...");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Oumar");
         vet1.setLastName("Kamissoko");
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Mariam");
         vet2.setLastName("Kanté");
 
